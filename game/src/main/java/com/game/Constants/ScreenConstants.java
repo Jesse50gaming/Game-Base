2443c9744e35028d30d3205466bc2ea0c77678c6
Jesse50gaming <128247303+Jesse50gaming@users.noreply.github.com>
2026-03-28 18:35:13 -0500

    start

---
+++ game/src/main/java/com/game/Constants/ScreenConstants.java
@@ -0,0 +1,14 @@
+package com.game.Constants;
+
+public final class ScreenConstants {
+
+    public final static float scale = 2;
+    public final static int baseScreenWidth = 800;
+    public final static int baseScreenHeight = 600;
+
+    public final static double screenWidth = baseScreenWidth * scale;
+    public final static double screenHeight = baseScreenHeight * scale;
+
+    public final static int FPS = 60;
+
+}
---
+++ game/src/main/java/com/game/Game.java
@@ -0,0 +1,16 @@
+package com.game;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements Runnable{
+
+    public Game() {
+
+    }
+
+    @Override
+    public void run() {
+        
+    }
+
+}
