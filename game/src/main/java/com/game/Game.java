cab4b32642e0f796c35e8b441b0aac19e94db93d
Jesse50gaming <128247303+Jesse50gaming@users.noreply.github.com>
2026-03-28 19:51:45 -0500

    S

--- game/src/main/java/com/game/Game.java
+++ game/src/main/java/com/game/Game.java
@@ -8,37 +8,43 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import com.game.Constants.ScreenConstants;
-
+import com.game.Input.KeyHandler;
+import com.game.Player.Player;
 
 public class Game extends JPanel implements Runnable{
 
     private Thread gameThread;
 
-   
+    private KeyHandler keyHandler;
+    private Player player;
+
     public Game() {
-       
+        keyHandler = new KeyHandler();
         this.setPreferredSize(new Dimension((int) ScreenConstants.screenWidth, (int) ScreenConstants.screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
         this.setFocusable(true);
         this.requestFocusInWindow();
+        addKeyListener(keyHandler);
         init();
     }
 
     private void init() {
-      
+        player = new Player(this);
     }
 
     private void update() {
-       
+        keyHandler.update();
+        player.update();
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         Graphics2D g2 = (Graphics2D)g;
-     
         
+        player.paint(g2);
+        //
         
     }
 
@@ -81,6 +87,8 @@ public class Game extends JPanel implements Runnable{
         gameThread.start();
     }
 
- 
+    public KeyHandler getKeyHandler() {
+        return keyHandler;
+    }
 
 }
